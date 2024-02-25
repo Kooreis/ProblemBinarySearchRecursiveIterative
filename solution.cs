20a5641800f87805f283fb6750573c46f0df1fba
Kooreis <kooreis.official@gmail.com>
2024-02-25 11:35:09 -0600

    This is the second part of the script where a function named 'binarySearchRecursive' is defined. This function performs a binary search on an array recursively. It takes an array, a number, a start index, and an end index as arguments, and returns true if the number is found in the array, false otherwise.

--- solution.cs
+++ solution.cs
@@ -1,10 +1,7 @@
-function binarySearchIterative(arr, x) {
-    let start=0, end=arr.length-1;
-    while (start<=end){
-        let mid=Math.floor((start + end)/2);
-        if (arr[mid]===x) return true;
-        else if (arr[mid] < x) start = mid + 1;
-        else end = mid - 1;
-    }
-    return false;
+function binarySearchRecursive(arr, x, start, end) {
+    if (start > end) return false;
+    let mid=Math.floor((start + end)/2);
+    if (arr[mid]===x) return true;
+    if(arr[mid] > x) return binarySearchRecursive(arr, x, start, mid-1);
+    else return binarySearchRecursive(arr, x, mid+1, end);
 }
